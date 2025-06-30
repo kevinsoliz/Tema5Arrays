ea8c16ab1f74147c9e5dde61de4496bc2ebf6308
Kevin Solz <kevinsoliz10@gmail.com>
2025-06-30 17:47:47 +0200

    Vamos con los arrays

---
+++ src/Actividades/Actividad01.java
@@ -0,0 +1,13 @@
+package Actividades;
+
+public class Actividad01 {
+    public static void main(String[] args) {
+        int[] enteros = new int[5];
+        double[] decimales = new double[5];
+        boolean[] buleanos = new boolean[5];
+
+        System.out.println(enteros);
+        System.out.println(decimales);
+        System.out.println(buleanos);
+    }
+}
