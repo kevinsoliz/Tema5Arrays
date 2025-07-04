ebf63d912237028beb68bec341fc52b58a15d27b
Kevin Solz <kevinsoliz10@gmail.com>
2025-07-04 18:25:58 +0200

    El 0 ya estaba dentro del array, había que empezar con un array vacío.

--- src/Practica/Ejercicio2.java
+++ src/Practica/Ejercicio2.java
@@ -9,8 +9,8 @@ import java.util.Arrays;
 public class Ejercicio2 {
     public static void main(String[] args) {
         int[] original = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        original = desordenar(original);
-        System.out.println(Arrays.toString(original));
+        int[] alea = arrayAleatorios(5);
+        System.out.println(Arrays.toString(alea));
 
     }
     static int[] desordenar(int[] original){
@@ -25,17 +25,17 @@ public class Ejercicio2 {
 
 
     static int[] arrayAleatorios(int longitud){
-       int [] arrayAleatorios = new int[longitud];
+       int [] arrayAleatorios = new int[0];
        int contador = 0;
-
        while(contador < longitud){
-           int aleatorio = (int) (Math.random() * arrayAleatorios.length);
-           if(!estaDentro(arrayAleatorios, aleatorio)){
-               arrayAleatorios[contador] = aleatorio;
+           int aleaotorio =  (int) (Math.random() * longitud);
+           if(!estaDentro(arrayAleatorios, aleaotorio)){
+               arrayAleatorios = Arrays.copyOf(arrayAleatorios, arrayAleatorios.length + 1);
+               arrayAleatorios[arrayAleatorios.length - 1] = aleaotorio;
                contador++;
            }
-       }
 
+       }
        return arrayAleatorios;
     }
 
